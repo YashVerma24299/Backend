3335c0dec4701227d6e73957502a519caab6094e
Yash <ritikverma23@gmail.com>
2025-03-21 10:48:20 +0530

    Learn how to import files in node

---
+++ 1_Node.js/9_Import/script.js
@@ -0,0 +1,11 @@
+// In a project, we generally use either "require" or "import" to load modules
+//"require" select the entire module, while "import" allows you to select specific parts of a module.  
+
+import {sum,mul} from "./math.js"
+
+// When you console this, a warning will appear:  
+// Warning: To load an ES module, set "type": "module" in package.json or use the .mjs extension.  
+// To fix this, set "type": "module" in package.json.  
+
+// Go and search many library
+//  https://www.npmjs.com/
